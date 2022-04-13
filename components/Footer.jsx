3573ecb31c949a7641fc9a21a3d07184f64f9b3e
siino-xyz <b114807h@gmail.com>
2022-04-14 04:30:16 +0900

    [add] トップページのスタイル調整 レスポンシブ対応など

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,30 +4,35 @@ import styles from '../styles/components/footer.module.scss'
 
 export default function Footer() {
   return (
-    <>
-      <div>
-        <Link href='/' passHref>
-          <Image
-            src="/global/se-27_footer_logo.svg" 
-            width={338}
-            height={107}
-            alt="se-27_logo"
-            layout='intrinsic'
-          />
-        </Link>
-
-        <Link href='https://twitter.com/siino_webdev' passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image 
-            src="/global/twitter_icon.svg" 
-            width={35}
-            height={35}
-            alt="twitter_icon"
-            layout='intrinsic'
+    <div className={styles.footer}>
+      <div className={styles.noWrapFlex}>
+        <div className={styles.footerLogo}>
+          <Link href='/' passHref>
+            <Image
+              src="/global/se-27_footer_logo.svg" 
+              width={338}
+              height={107}
+              alt="se-27_logo"
+              layout='intrinsic'
             />
-          </a>
-        </Link>
+          </Link>
+        </div>
+
+        <div className={styles.twitterLogo}>    
+          <Link href='https://twitter.com/siino_webdev' passHref>
+            <a target="_blank" rel="noopener noreferrer">
+              <Image 
+              src="/global/twitter_icon.svg" 
+              width={35}
+              height={35}
+              alt="twitter_icon"
+              layout='intrinsic'
+              />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.footerLogo}></div>
       </div>
-    </>
+    </div>
   )
 }
