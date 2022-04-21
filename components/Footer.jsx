046f38bd46494f71dbc84519d3ae8a84b6563dd3
siino-xyz <b114807h@gmail.com>
2022-04-21 11:56:16 +0900

    [fix]トップページのスタイル微調整

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,38 +1,38 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from '../styles/components/footer.module.scss'
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/components/footer.module.scss";
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.noWrapFlex}>
         <div className={styles.footerLogo}>
-          <Link href='/' passHref>
+          <Link href="/" passHref>
             <Image
-              src="/global/se-27_footer_logo.svg" 
+              src="/global/se-27_footer_logo.svg"
               width={338}
               height={107}
               alt="se-27_logo"
-              layout='intrinsic'
+              layout="intrinsic"
             />
           </Link>
         </div>
 
-        <div className={styles.twitterLogo}>    
-          <Link href='https://twitter.com/siino_webdev' passHref>
+        {/* <div className={styles.twitterLogo}>
+          <Link href="https://twitter.com/siino_webdev" passHref>
             <a target="_blank" rel="noopener noreferrer">
-              <Image 
-              src="/global/twitter_icon.svg" 
-              width={35}
-              height={35}
-              alt="twitter_icon"
-              layout='intrinsic'
+              <Image
+                src="/global/twitter_icon.svg"
+                width={35}
+                height={35}
+                alt="twitter_icon"
+                layout="intrinsic"
               />
             </a>
           </Link>
-        </div>
-        <div className={styles.footerLogo}></div>
+        </div> */}
+        <div className={styles.copylight}>© 2022 SE-27</div>
       </div>
     </div>
-  )
+  );
 }
