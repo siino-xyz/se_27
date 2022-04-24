b5fdd9f006591e4c62ecd2fcde8918610f8bb5b0
siino-xyz <b114807h@gmail.com>
2022-04-24 16:22:39 +0900

    [fix]imgixの数値を変更

--- components/CustomImage.tsx
+++ components/CustomImage.tsx
@@ -1,11 +1,11 @@
 import base64url from "base64url";
 
 const CustomImage = (baseImageUrl: string, title: string) => {
-  const ogImageUrl = `${baseImageUrl}?w=1200&h=1200&blend64=${base64url(
-    `https://assets.imgix.net/~text?txtsize=48&txt-color=009688&w=1120&txt-align=middle&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
+  const ogImageUrl = `${baseImageUrl}?w=640&h=400&blend64=${base64url(
+    `https://assets.imgix.net/~text?txtsize=38&w=550&txt-color=EDEDED&txt-align=middle&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
       title
     )}`
-  )}&blend-mode=normal&blend-align=top,left&blend-x=80&blend-y=180`;
+  )}&blend-mode=normal&blend-x=middle&blend-y=middle&fit=crop&usm=20`;
 
   return { ogImageUrl };
 };
