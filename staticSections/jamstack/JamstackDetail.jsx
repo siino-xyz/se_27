6c93d3b09362e1678e6c2f49275b7248faedb79c
siino-xyz <b114807h@gmail.com>
2022-04-29 23:05:13 +0900

    [fix]jamstack page styles

--- staticSections/jamstack/JamstackDetail.jsx
+++ staticSections/jamstack/JamstackDetail.jsx
@@ -17,10 +17,18 @@ const JamstackDetail = () => {
         </div> */}
         <main className={styles.sectionInner}>
           <div className={styles.sectionTitle}>
-            <h2 className={styles.h2}>Jamstackとはなにか</h2>
+            <h2 className={styles.h2}>What's Jamstack?</h2>
+            <div className={styles.subscript}>Jamstackとはなにか</div>
           </div>
 
-          <div>
+          <div className={styles.grid}>
+            <Image
+              src="/jamstack/jamstackLogo.svg"
+              width={383}
+              height={129}
+              layout="intrinsic"
+              alt="jamstackLogo"
+            />
             <div>
               <p>
                 jamstackとは、JavaScript・API・Markup(HTML/CSS)の3つをベースとしてウェブサイトを構築する、比較的新しいアーキテクチャのひとつです。
@@ -33,18 +41,12 @@ const JamstackDetail = () => {
                 このページでは、従来のウェブサイトとJamstackの違いや、Jamstackがどういった場面に向いているのかについて解説していきます。まずはJamstackの特徴を簡単にご紹介します。
               </p>
             </div>
-            <Image
-              src="/jamstack/jamstackLogo.svg"
-              width={383}
-              height={129}
-              layout="intrinsic"
-              alt="jamstackLogo"
-            />
+          </div>
+          <div className={styles.featureTitle}>
+            <span>Feature 1</span>
+            <h3>表示速度が高速</h3>
           </div>
           <div>
-            <div className={styles.h3Title}>
-              <h3>特徴１.表示速度が高速</h3>
-            </div>
             <p>
               Jamstackは、
               すべてのページがあらかじめ生成された状態で配信されるという特徴に加え、CDN（コンテンツ・デリバリー・ネットワーク）経由でのホスティングを行うため、コンテンツの表示速度が高速です。
@@ -53,18 +55,21 @@ const JamstackDetail = () => {
               ユーザのリクエストに応じて都度ページを生成する場合は「動的」といいます。
             </p>
           </div>
+          <div className={styles.featureTitle}>
+            <span>Feature 2</span>
+            <h3>高セキュリティ</h3>
+          </div>
           <div>
-            <div className={styles.h3Title}>
-              <h3>特徴２.セキュリティリスクが低い</h3>
-            </div>
             <p>
               ハッカーの攻撃の的となる、サーバーやデータベースがウェブページから切り離されているため、従来型のウェブサイトよりもセキュリティホールが圧倒的に少ないというメリットがあります。
             </p>
           </div>
+
+          <div className={styles.featureTitle}>
+            <span>Feature 3</span>
+            <h3>スケーラブルな運用が可能</h3>
+          </div>
           <div>
-            <div className={styles.h3Title}>
-              <h3>特徴３.スケーラブルな運用が可能</h3>
-            </div>
             <p>
               Jamstackで構築されたウェブサイトは複雑なバックエンドを持たないため、規模の変化に強いというメリットがあります。
               <br />
@@ -75,17 +80,27 @@ const JamstackDetail = () => {
 
         <main className={styles.sectionInner}>
           <div className={styles.sectionTitle}>
-            <h2 className={styles.h2}>図で見る、従来との違い</h2>
+            <h2 className={styles.h2}>Difference</h2>
+            <div className={styles.subscript}>従来との違い</div>
           </div>
           <p>
             現在主流のウェブサイト配信の方法と、Jamstackの配信方法では具体的に何が違うのでしょうか？
             簡単な図で解説しています。
           </p>
-          <div className={styles.diff}>
-            <div className={styles.h3Title}>
+          <div>
+            <div className={styles.featureTitle}>
               <h3>現在主流のウェブサイト配信</h3>
             </div>
-            <div className={styles.diffContent}>
+            <div>
+              <div>
+                <Image
+                  src="/jamstack/lampstack.svg"
+                  width={456}
+                  height={497}
+                  layout="intrinsic"
+                  alt="lanpstack"
+                />
+              </div>
               <div>
                 <p>
                   サーバは、ユーザーからのリクエストがあるたびに必要なデータをデータベースへ取得しに行き、ブラウザに返却するという流れです。この配信方法には、大きく2つのネックがあります。
@@ -103,20 +118,21 @@ const JamstackDetail = () => {
                   パフォーマンスチューニングやセキュリティ施策を正しく行っていれば基本的には問題ないものの、維持管理のコストがかかってくることもあり、十分な対策が行えていないウェブサイトも少なくありません。
                 </p>
               </div>
-              <div className={styles.gridReverse}>
+            </div>
+
+            <div className={styles.featureTitle}>
+              <h3>Jamstackのウェブサイト配信</h3>
+            </div>
+            <div>
+              <div>
                 <Image
-                  src="/jamstack/lampstack.svg"
-                  width={456}
+                  src="/jamstack/serverLessStack.svg"
+                  width={455}
                   height={497}
                   layout="intrinsic"
                   alt="lanpstack"
                 />
               </div>
-            </div>
-            <div className={styles.h3Title}>
-              <h3>Jamstackのウェブサイト配信</h3>
-            </div>
-            <div className={styles.diffContent}>
               <div>
                 <div>
                   <p>
@@ -134,28 +150,20 @@ const JamstackDetail = () => {
                   </p>
                 </div>
               </div>
-              <div className={styles.gridReverse}>
-                <Image
-                  src="/jamstack/serverLessStack.svg"
-                  width={455}
-                  height={497}
-                  layout="intrinsic"
-                  alt="lanpstack"
-                />
-              </div>
             </div>
           </div>
         </main>
 
         <main className={styles.sectionInner}>
           <div className={styles.sectionTitle}>
-            <h2 className={styles.h2}>Jamstackを導入すべきか</h2>
+            <h2 className={styles.h2}>When to use Jamstack?</h2>
+            <div className={styles.subscript}>Jamstackを導入すべきか</div>
           </div>
 
-          <div className={styles.shouldbe}>
-            <div className={styles.shouldbeContent}>
+          <div>
+            <div>
               <div>
-                <div className={styles.h3Title}>
+                <div className={styles.featureTitle}>
                   <h3>Jamstackが向いているケース</h3>
                 </div>
                 <p>
@@ -178,11 +186,12 @@ const JamstackDetail = () => {
                 />
               </div> */}
             </div>
-            <div className={styles.shouldbeContent}>
+            <div>
               <div>
-                <div className={styles.h3Title}>
+                <div className={styles.featureTitle}>
                   <h3>Jamstackが向いていないケース</h3>
                 </div>
+
                 <p>
                   CDNのキャッシュサーバに一時的にデータを保存して配信するという構造は効率的である反面、即座な情報更新が難しいというデメリットがあります。そのため、以下のようなケースではJamstackを採用しにくいです。
                 </p>
@@ -207,10 +216,11 @@ const JamstackDetail = () => {
 
         <main className={styles.sectionInner}>
           <div className={styles.sectionTitle}>
-            <h2 className={styles.h2}>Jamstackなホームーページを作るなら</h2>
+            <h2 className={styles.h2}>Contact us</h2>
+            <div className={styles.subscript}>SE-27に相談してみる？</div>
           </div>
 
-          <div className={styles.cta}>
+          <div>
             <p>
               SE-27ではJamstackアーキテクチャを用いた、高速かつ安全性の高いウェブサイト開発を専門としています。
               単に速いサイトを作るだけではありません。運用スタイルや規模感に合わせて柔軟に
--- staticSections/toppage/BigTypo.jsx
+++ staticSections/toppage/BigTypo.jsx
@@ -5,20 +5,22 @@ export default function Bigtypo() {
   return (
     <section className={styles.sectionContainer}>
       <main className={styles.sectionInner}>
-        <Image
-          src="/top/top-largeTypo.svg"
-          width={588}
-          height={355}
-          layout="intrinsic"
-          alt="top-largeTypo"
-        />
+        <div className={styles.bigTypo}>
+          <Image
+            src="/top/top-largeTypo.svg"
+            width={588}
+            height={355}
+            layout="intrinsic"
+            alt="top-largeTypo"
+          />
 
-        <div>
-          <p>
-            大量の情報がめまぐるしいスピードで消化されていく現代ネット社会では、ユーザーが1つのコンテンツに使える時間は限られており、そんなに待ってくれない。
-            <br />
-            SE-27は、「ユーザーにいちばん伝えたいことをできる限りシンプルに、そして刹那の速さで届ける」というコンセプトのもと、高速で安全なウェブサイトをデザイン・開発します。
-          </p>
+          <div>
+            <p>
+              大量の情報がめまぐるしいスピードで消化されていく現代ネット社会では、ユーザーが1つのコンテンツに使える時間は限られており、そんなに待ってくれない。
+              <br />
+              SE-27は、「ユーザーにいちばん伝えたいことをできる限りシンプルに、そして刹那の速さで届ける」というコンセプトのもと、高速で安全なウェブサイトをデザイン・開発します。
+            </p>
+          </div>
         </div>
       </main>
     </section>
