7892fbda33676e5259d9dc5cb94442ff337c4b0c
siino-xyz <b114807h@gmail.com>
2022-05-03 02:05:04 +0900

    [fix] gtag setting

--- pages/_app.js
+++ pages/_app.js
@@ -2,35 +2,12 @@ import "../styles/globals.scss";
 import "../styles/reset.css";
 import Head from "next/head";
 
-import { GA_ID, existsGaId } from "../libs/gtag";
-
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return getLayout(
     <>
       <Head>
-        <>
-          {existsGaId && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
-                }}
-              />
-            </>
-          )}
-        </>
         <title>Jamstack専門のホームページ制作 | SE-27.com</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_ID, existsGaId } from "../libs/gtag";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          {existsGaId && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              />
+              {/* <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-01Q62C3NEV"
+              ></script> */}
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+                }}
+              />
+            </>
+          )}
           <link
             href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@700&family=Zen+Kaku+Gothic+New:wght@400;700&&display=swap"
             rel="stylesheet"
