b793d104814a0ca458c8c8c8f4723147d6f7f9fd
siino-xyz <b114807h@gmail.com>
2022-06-03 10:31:24 +0900

    nextogpをコメントアウト

--- layouts/Meta.js
+++ layouts/Meta.js
@@ -1,13 +1,13 @@
-import NextOgp from "../components/NextOgp";
+// import NextOgp from "../components/NextOgp";
 import useAsPath from "../hooks/useAsPath";
 export default function Meta({ children }) {
   const { path } = useAsPath();
   return (
     <>
-      <NextOgp
+      {/* <NextOgp
         pagedescription={"Jamstack専門のホームページ制作 SE-27（せつな）"}
         pagepath={path}
-      />
+      /> */}
 
       <div>{children}</div>
     </>
